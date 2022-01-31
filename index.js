0627f0781288036ee1d5083687f74df13dec66a7
Denis Pavlovich <denishka2112@gmail.com>
2022-01-31 20:42:47 +0200

    trening js

---
+++ index.js
@@ -0,0 +1,16 @@
+/*function numberReduction(){
+for(let number = 25; 0 <= number; number-- )
+console.log(number);
+}*/
+/*function number(){
+for (let counter = 10; counter <= 50; counter += 5){
+
+  console.log(counter);
+};
+}*/
+
+for(let num = 0, sum = 0; num <= 100; num++ ){
+  
+console.log(sum+=num);
+}
+
