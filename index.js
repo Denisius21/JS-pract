1ee9d3e192d9f11291e82bcab7ba317f6177cab3
Denis Pavlovich <denishka2112@gmail.com>
2022-02-01 20:49:48 +0200

    Add object

--- index.js
+++ index.js
@@ -9,8 +9,33 @@ for (let counter = 10; counter <= 50; counter += 5){
 };
 }*/
 
-for(let num = 0, sum = 0; num <= 100; num++ ){
+/*for(let num = 0, sum = 0; num <= 100; num++ ){
   
 console.log(sum+=num);
-}
+}*/
 
+/*const student = {
+  studentName : 'Stepan',
+  studentLastName : 'Fedorov',
+  age : 24,
+  weight: 70,
+  education : 'school',
+  hobby : 'football',
+  colorYeas : 'grey',
+}
+function greet(person){
+  return 'Hello '+ student.studentName +' '+student.studentLastName;
+}*/
+
+function Car (manufacturer, model, color, releaseYear, fuelConsumpition,volume){
+  this.rides = function(){
+    return 'rides';
+  };
+  this.manufacturer = manufacturer;
+  this.model = model;
+  this.color = color;
+  this.releaseYear = releaseYear;
+  this.fuelConsumpition = fuelConsumpition;
+  this.volume = volume;
+}
+const vaz = new Car('autoVaz', 'vaz2112', 'green', 2006, 9, 50);
